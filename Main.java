69bf330f4e0189e36a75b6240453be7ffbf7746c
Hoang Pham <hoangpham_2023@depauw.edu>
2020-04-19 14:01:19 -0400

    potential world added

--- Main.java
+++ Main.java
@@ -58,4 +58,64 @@ class Main {
             }
         }
     }
+    
+/*
+ *  private static void createWorld()
+    {
+    	Location kitchen = new Location("Kitchen", "Cooking place");
+    	kitchen.addItem(new Item("Kettle", "Appliance", "Perfect for brewing hot tea and coffee"));
+    	kitchen.addItem(new Item("Turkey", "Food", "Good but not the best meat type"));
+    	kitchen.addItem(new Item("Plate", "Dinnerware", "Shiny silver ellipse object"));
+    	kitchen.addItem(new Item("Toaster", "Appliance", "Stainless Classic Toaster"));
+    	kitchen.addItem(new ContainerItem("Toolbox", "Container", "A wooden box"));
+    	((ContainerItem) kitchen.getItem("Toolbox")).addItem(new Item("Knife", "Cutlery", "Japanese Knife"));
+    	((ContainerItem) kitchen.getItem("Toolbox")).addItem(new Item("Cutting Board", "Tool", "Plastic Board"));
+    
+		Location bedroom = new Location("Bedroom", "Sleeping place");
+		bedroom.addItem(new Item("Bed", "Furniture", "Softest Bed on Earth"));
+		bedroom.addItem(new Item("Wardrobe", "Furniture", "Old Wardrobe from the 90s"));
+		bedroom.addItem(new Item("Lamp", "Furniture", "Rich textures, highlighted in silver leaf"));
+		bedroom.addItem(new Item("Crib", "Furniture", "Easy to re-style and re-arrange"));
+		bedroom.addItem(new ContainerItem("Dresser", "Container", "Three-drawer chest"));
+    	((ContainerItem) bedroom.getItem("Dresser")).addItem(new Item("Tee", "Clothes", "H&M Slim Fit"));
+    	((ContainerItem) bedroom.getItem("Dresser")).addItem(new Item("Hoodie", "Clothes", "Soft, brushed inside"));
+    	
+		Location hallway = new Location("Hallway", "Path to go outside");
+		hallway.addItem(new Item("Ball", "Toy", "Ultra-durable and Chewy"));
+		hallway.addItem(new Item("Golf Stick", "Tool", "Fancy Golf Stick"));
+		hallway.addItem(new Item("Rug", "Decor", "Soft and Luxurious"));
+		hallway.addItem(new Item("Sword", "Decor", "An Old Sword from the ancient Greece"));
+		hallway.addItem(new ContainerItem("Chest", "Container", "Made of 100% natural bamboo"));
+    	((ContainerItem) hallway.getItem("Chest")).addItem(new Item("Adidas Shoes", "Shoes", "8.5 White Basketball Shoes"));
+    	((ContainerItem) hallway.getItem("Chest")).addItem(new Item("Fur Slippers", "Slippers", "Grey, Ombre"));
+    	
+		Location bathroom = new Location("Bathroom", "Shower");
+		bathroom.addItem(new Item("Razor", "Blade", "Unisex, Double sided, Handmade Metal"));
+		bathroom.addItem(new Item("Hairdryer", "Tool", "Salon-quality hairdryer"));
+		bathroom.addItem(new Item("Towels", "Towel", "Extremely Soft on Face"));
+		bathroom.addItem(new Item("Waste Basket", "Basket", "Open, Steel in Black, with small capacity"));
+		bathroom.addItem(new ContainerItem("Caddy", "Container", "A wooden box"));
+    	((ContainerItem) bathroom.getItem("Caddy")).addItem(new Item("Shampoo", "Tool", "Expensive Shampoo"));
+    	((ContainerItem) bathroom.getItem("Caddy")).addItem(new Item("Conditioner", "Tool", "Empty bottle of Hair Conditioner"));
+    	
+		// connect all 4 to some of the others
+		kitchen.connect("north", hallway);
+		kitchen.connect("east", bathroom);
+		
+		hallway.connect("south", kitchen);
+		
+		bathroom.connect("west", kitchen);
+		bathroom.connect("south", bedroom);
+		
+		bedroom.connect("north", bathroom);
+		
+		//assign currentLocation to kitchen
+		currentLocation = kitchen;
+		
+		//construct myInventory
+		myInventory = new ContainerItem("Backpack", "Container", "My own storage");
+		
+		
+    }
+ */
 }
